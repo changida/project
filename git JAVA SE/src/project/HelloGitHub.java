84b403b45e34cce05a239ac0869372760dd3535d
sec <sec@class-16>
2017-09-01 12:36:21 +0900

    헬로우깃 테스트

---
+++ git JAVA SE/src/project/HelloGitHub.java
@@ -0,0 +1,26 @@
+/**
+ * 작 성 일 : 2017. 9. 1.
+ * 파 일 명 : HelloGitHub.java
+ * 위    치 : 
+ * 작성시간 : 오후 12:30:38
+ * 작 성 자 : sec
+ */
+package project;
+
+/**
+ * @author sec
+ *
+ */
+public class HelloGitHub {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello GitHub");
+
+	}
+
+}
